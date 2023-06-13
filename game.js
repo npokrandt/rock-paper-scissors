848363e6e5b854487ef9fdd84cafd4677de58c63
npokrandt <npokrandt919@gmail.com>
2023-06-12 20:58:35 -0500

    human and comp choice added

--- game.js
+++ game.js
@@ -1,5 +1,38 @@
-console.log("Hello RPS")
 
-var rock = {
-    type: "igneous"
-}
+
+    var wins = 0, 
+        losses = 0, 
+        ties = 0
+
+    var choices = ["R", "P", "S"]
+
+    var humanChoice = prompt("Choose R, P, or S")
+    console.log(humanChoice)
+
+    var computerChoice = choices[Math.floor(Math.random() * 3)]
+
+    console.log(computerChoice)
+
+// players choose R, P, S
+  // computer randomly chooses
+
+// compare choices
+  // if humanChoice is S and computerChoice is P OR
+  // if humanChoice is R and computerChoice is S OR
+  // if humanChoice is P and computerChoice is R
+    // alert that we won
+    // increase wins
+  // if humanChoice === computerChoice
+    // alert that we tied
+    // increase ties 
+  // else
+    // alert that lost
+    // increase losses
+
+// ask user "do you want to play again?"
+  // if yes
+    // restart game
+  // if no
+    // exit game
+
+
